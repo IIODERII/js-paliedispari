1ec7c2903c74f8fa612b87063fdb9757de41e920
IIODERII <oderrisi@gmail.com>
2023-10-18 06:46:07 +0200

    added iife on script.js

--- js/script.js
+++ js/script.js
@@ -1,62 +1,66 @@
-const paliBtn = document.getElementById('palBtn');
+(function oderPalindromi(){
+    const paliBtn = document.getElementById('palBtn');
 
-paliBtn.addEventListener('click', function(){
-    const userWord = document.getElementById('word').value;
-    const msg = document.querySelector('#pali-side .alert');
+    paliBtn.addEventListener('click', function(){
+        const userWord = document.getElementById('word').value;
+        const msg = document.querySelector('#pali-side .alert');
 
-    messageReset(msg);
+        messageReset(msg);
 
-    if(isPalindroma(userWord)){
-        msg.classList.add('bg-success');
-        msg.innerHTML = '&check; Sì ' + userWord +' è una parola palindroma!!'
-    }else{
-        msg.classList.add('bg-danger');
-        msg.innerHTML = '&cross; No ' + userWord +' non è una parola palindroma!!'
-    }
-    msg.classList.remove('d-none')
-    
-})
-
-const dispBtn = document.getElementById('dispBtn');
-const data = document.createElement('div');
-
-dispBtn.addEventListener('click', function(){
-    const userNumber = parseInt(document.getElementById('numb').value);
-    const msg = document.querySelector('#odd-even-side .alert');
-    const userSelect =parseInt(document.getElementById('paridispari').value);
-
-    const computerNumber = getRndInteger(1,5) ;
-    const pari = 0;
-    const dispari = 1;
-    let somma ;
-
-    messageReset(msg);
-
-    if(isBetween1and5(userNumber)){
-        
-        somma = userNumber + computerNumber;
-
-        if(userSelect === pari && isEven(somma)){
+        if(isPalindroma(userWord)){
             msg.classList.add('bg-success');
-            msg.innerHTML = 'Complimenti hai vinto!!' ;
-        }else if(userSelect === dispari && isEven(somma)){
+            msg.innerHTML = '&check; Sì ' + userWord +' è una parola palindroma!!'
+        }else{
             msg.classList.add('bg-danger');
-            msg.innerHTML = 'Peccato hai perso :(' ;
-        }else if(userSelect === pari && !isEven(somma)){
-            msg.classList.add('bg-danger');
-            msg.innerHTML = 'Peccato hai perso :(' ;
-        }else if(userSelect === dispari && !isEven(somma)){
-            msg.classList.add('bg-success');
-            msg.innerHTML = 'Complimenti hai vinto!!' ;
+            msg.innerHTML = '&cross; No ' + userWord +' non è una parola palindroma!!'
         }
+        msg.classList.remove('d-none')
+        
+    })
+}());
 
-        data.className= 'fs-2 fw-bold py-4'
-        data.innerHTML = userNumber + '+' + computerNumber + '=' + somma ;
-        document.getElementById('message-container').prepend(data);
-    }else{
-        msg.classList.add('bg-warning')
-        msg.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> '+ userNumber + ' non è compreso tra 1 e 5 <i class="fa-solid fa-triangle-exclamation"></i>'
-    }
-
-    msg.classList.remove('d-none');
-})
+(function oderOddOrEven(){
+    const dispBtn = document.getElementById('dispBtn');
+    const data = document.createElement('div');
+    
+    dispBtn.addEventListener('click', function(){
+        const userNumber = parseInt(document.getElementById('numb').value);
+        const msg = document.querySelector('#odd-even-side .alert');
+        const userSelect =parseInt(document.getElementById('paridispari').value);
+    
+        const computerNumber = getRndInteger(1,5) ;
+        const pari = 0;
+        const dispari = 1;
+        let somma ;
+    
+        messageReset(msg);
+    
+        if(isBetween1and5(userNumber)){
+            
+            somma = userNumber + computerNumber;
+    
+            if(userSelect === pari && isEven(somma)){
+                msg.classList.add('bg-success');
+                msg.innerHTML = 'Complimenti hai vinto!!' ;
+            }else if(userSelect === dispari && isEven(somma)){
+                msg.classList.add('bg-danger');
+                msg.innerHTML = 'Peccato hai perso :(' ;
+            }else if(userSelect === pari && !isEven(somma)){
+                msg.classList.add('bg-danger');
+                msg.innerHTML = 'Peccato hai perso :(' ;
+            }else if(userSelect === dispari && !isEven(somma)){
+                msg.classList.add('bg-success');
+                msg.innerHTML = 'Complimenti hai vinto!!' ;
+            }
+    
+            data.className= 'fs-2 fw-bold py-4'
+            data.innerHTML = userNumber + '+' + computerNumber + '=' + somma ;
+            document.getElementById('message-container').prepend(data);
+        }else{
+            msg.classList.add('bg-warning')
+            msg.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> '+ userNumber + ' non è compreso tra 1 e 5 <i class="fa-solid fa-triangle-exclamation"></i>'
+        }
+    
+        msg.classList.remove('d-none');
+    })
+}());
--- js/utility.js
+++ js/utility.js
@@ -1,27 +1,30 @@
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-function isPalindroma(parola){
-    return (reversedWord(parola).toLowerCase() === parola.toLowerCase()) ? true : false;
-}
-
-function reversedWord(parola) {
-    let reversed = '' ;
-    for(let i = parola.length - 1 ; i >= 0 ; i--){
-        reversed += parola[i];
+//(function oderUtility(){
+    
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
-    return reversed
-}
-
-function messageReset(myEl){
-    myEl.className = 'alert d-none p-4 fw-bold mt-3 text-center';
-}
-
-function isBetween1and5(number){
-    return (number >= 1 && number <= 5) ? true : false;
-}
-
-function isEven(num) {
-    return (num % 2 === 0) ? true : false;
-}
+    
+    function isPalindroma(parola){
+        return (reversedWord(parola).toLowerCase() === parola.toLowerCase()) ? true : false;
+    }
+    
+    function reversedWord(parola) {
+        let reversed = '' ;
+        for(let i = parola.length - 1 ; i >= 0 ; i--){
+            reversed += parola[i];
+        }
+        return reversed
+    }
+    
+    function messageReset(myEl){
+        myEl.className = 'alert d-none p-4 fw-bold mt-3 text-center';
+    }
+    
+    function isBetween1and5(number){
+        return (number >= 1 && number <= 5) ? true : false;
+    }
+    
+    function isEven(num) {
+        return (num % 2 === 0) ? true : false;
+    }
+//}());
